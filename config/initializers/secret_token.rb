48cbaec329d3b1843e0e0d6d00ff03350ffac08d
bmirtich <bmirtich@users.noreply.github.com>
2014-07-14 00:38:54 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubbertest2::Application.config.secret_token = '296ef26b240b923b3ceab8118ad333327df4efa7c4d0a3c9d3b061b61cff4b00f7d5e067bdf053a7b8412ad92c914b65ca8a62e7c5c20dfb36c9e58367c5840f'
